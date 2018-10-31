e5026d7c4e3998dfbea8b0e98392e08dc1ba41fe
Russell Nyorere <ecodenhub@gmail.com>
2018-10-31 09:01:07 +0100

    Added a function to dynamically inject current year

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,13 @@
 // Global Variable
 let slideIndex = 0;
 
-// IFFE Init Call to function
+// IFFE Init Call to both functions
 (function init() {
     // Call image Carousel 
     imageCarousel();
+
+    // Call copy right year
+    copyRightYear();
 
 }());
 
@@ -19,4 +22,11 @@ function imageCarousel() {
     if (slideIndex > images.length) {slideIndex = 1} 
     images[slideIndex-1].style.display = "block"; 
     setTimeout(imageCarousel, 3000); // Change image every 3 seconds
+};
+
+// Function to get current year
+function copyRightYear() {
+    let currentYear = document.getElementById('year');
+    let year = new Date().getFullYear();
+    currentYear.innerHTML = year;
 };
