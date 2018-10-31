98e579f7c65c5d989dbd0e946602210b28047fa2
Russell Nyorere <ecodenhub@gmail.com>
2018-10-31 08:58:20 +0100

    Created script.js and write a function to display image carousel

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// Global Variable
+let slideIndex = 0;
+
+// IFFE Init Call to function
+(function init() {
+    // Call image Carousel 
+    imageCarousel();
+
+}());
+
+// Function to displays Image Carousel
+function imageCarousel() {
+    let i;
+    let images = document.getElementsByClassName("banner__img-slides");
+    for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > images.length) {slideIndex = 1} 
+    images[slideIndex-1].style.display = "block"; 
+    setTimeout(imageCarousel, 3000); // Change image every 3 seconds
+};
